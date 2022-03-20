716bb6984e4ea357c15f4c5cf661dfae5561e8b8
meetm473 <edm18b037@iiitdm.ac.in>
2022-03-20 17:21:56 +0530

    Finished example code

--- r2b_control/src/example_task/main.cpp
+++ r2b_control/src/example_task/main.cpp
@@ -1,10 +1,29 @@
 #include "arduinotypeinterface.h"
 
+bool task_done = false;
+
 void setup(void){
-	ROS_INFO_STREAM("Setup done!");
+	ROS_INFO_STREAM("Example task: This task will go towards the top box, and then go to the place where it needs to be placed");
 }
 
 void loop(void){
-	ROS_INFO_STREAM("Testing");
-	delay(1000);
+	if(!task_done){
+		ROS_INFO_STREAM("Starting the task...");
+		delay(1000);
+
+		ROS_INFO_STREAM("Reached pose 1");
+		r2b.setJointAngles(56.2, -47.56);
+		delay(2000);
+
+		ROS_INFO_STREAM("Reached pose 2");
+		r2b.setJointAngles(90, -50);
+		delay(2000);
+
+		ROS_INFO_STREAM("Reached pose 3");
+		r2b.setJointAngles(41.87, -89.38);
+		delay(2000);
+
+		ROS_INFO_STREAM("Task done");
+		task_done = true;
+	}
 }
