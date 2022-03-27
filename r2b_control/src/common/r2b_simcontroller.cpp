54b83760169fb22f9c678fece9ebd8949c8051f1
meetm473 <edm18b037@iiitdm.ac.in>
2022-03-27 21:19:30 +0530

    Refined r2b control

--- r2b_control/src/common/r2b_simcontroller.cpp
+++ r2b_control/src/common/r2b_simcontroller.cpp
@@ -3,9 +3,9 @@
 R2bSimController::R2bSimController(ros::NodeHandle &nh){
 	box_attach_rqst_sent_.data = false;
 
-	jnt_1_pub_ = nh.advertise<std_msgs::Float64>("/r2b/jnt_1_controller/command", 1);
-	jnt_2_pub_ = nh.advertise<std_msgs::Float64>("/r2b/jnt_2_controller/command", 1);
-	box_attach_pub_ = nh.advertise<std_msgs::Bool>("/r2b/connect_box", 1);
+	jnt_1_pub_ = nh.advertise<std_msgs::Float64>("/r2b/jnt_1_controller/command", 1, true);
+	jnt_2_pub_ = nh.advertise<std_msgs::Float64>("/r2b/jnt_2_controller/command", 1, true);
+	box_attach_pub_ = nh.advertise<std_msgs::Bool>("/r2b/connect_box", 1, true);
 }
 
 void R2bSimController::setJointAngles(double _jnt_1 = 0, double _jnt_2 = 0){
@@ -14,6 +14,7 @@ void R2bSimController::setJointAngles(double _jnt_1 = 0, double _jnt_2 = 0){
 	
 	joint_angle_.data = _jnt_2 * DEG2RAD;
 	jnt_2_pub_.publish(joint_angle_);
+	ros::spinOnce();
 }
 
 void R2bSimController::toggleGrasp(){
@@ -25,4 +26,5 @@ void R2bSimController::toggleGrasp(){
 	}
 
 	box_attach_pub_.publish(box_attach_rqst_sent_);
+	ros::spinOnce();
 }
